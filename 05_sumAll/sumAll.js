20473e9fe44d1384df3ba1d9443b33c8b3bd0334
Jonathan Schmidt <jona.schmidt99@web.de>
2024-05-26 17:53:05 +0200

    Implement function to sum all numbers in specific range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    typeof num1 !== 'number' ||
+    typeof num2 !== 'number'
+  ) {
+    return 'ERROR';
+  }
 
+  let lowerBound = num1;
+  let upperBound = num2;
+
+  if (num1 > num2) {
+    lowerBound = num2;
+    upperBound = num1;
+  }
+
+  let sum = 0;
+
+  for (let i = lowerBound; i <= upperBound; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
